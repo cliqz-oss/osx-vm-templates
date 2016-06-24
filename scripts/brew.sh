6f92511dbdef0f44d52d5b1a98e427e3511cd69d
Krzysztof Jan Modras <krzysztof.modras@gmail.com>
2016-06-24 14:11:24 +0200

    fixing brew script to use proper user

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -2,10 +2,10 @@
 
 set -eo pipefail
 
-: | sudo -u travis -H ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+: | sudo -u vagrant -H ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-sudo -u travis -H /bin/bash --login -c \
+sudo -u vagrant -H /bin/bash --login -c \
 	'brew update'
 
-sudo -u travis -H /bin/bash --login -c \
+sudo -u vagrant -H /bin/bash --login -c \
     'brew install carthage'
